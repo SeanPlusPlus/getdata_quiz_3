3d97cf74237dbeea29fb885a6cb2574e979b6b48
Sean Stephenson <sms@pixar.com>
2015-02-21 12:16:34 -0800

    q2 quantile foo

--- q2.R
+++ q2.R
@@ -12,7 +12,7 @@ getImage <- function() {
 ################################################################################
 
   # install packages
-  install.packages("jpeg")  ## if necessary
+  install.packages("jpeg")
   library(jpeg)
 
   # add data dir
@@ -23,7 +23,7 @@ getImage <- function() {
   # fetch and return
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
   download.file(fileUrl, destfile = "./data/data2.jpg", method = "curl")
-  img <- readJPEG("./data/data2.jpg")
+  img <- readJPEG("./data/data2.jpg", native = TRUE)
   return(img)
 }
 
@@ -34,3 +34,7 @@ main <- function() {
 }
 
 main()
+# img <- source('q2.R')
+# data <- data.matrix(img)
+# answer <- quantile(data[[1]], c(.3, .8))
+# print(answer)
